1e88d3ca7be35980ebea094a42e88379ff7c5268
kevinantoniovazquezperez <141761226+kevinantoniovazquezperez@users.noreply.github.com>
2023-10-23 08:29:22 -0600

    Add files via upload

---
+++ Proyecto final/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es-MEX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Arte Chiapaneca</title>
+    
+</head>
+
+<body>
+    <!--Menú-->
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+      <img src="https://ethic.es/wp-content/uploads/2022/12/vangog-2.jpg" alt="..." height="40">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Iniciar Sesión</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Registrarse</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Productos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Conocenos</a>
+        </li>
+    </div>
+  </div>
+</nav>
+<td>
+    <img src="https://media.istockphoto.com/id/1406317576/es/vector/calendario-de-los-antiguos-pueblos-mayas-en-forma-de-c%C3%ADrculo-y-s%C3%ADmbolos-de-civilizaciones.webp?s=1024x1024&w=is&k=20&c=11J88fTeZwhe6RcCN5eKcv53OLob2bXoBcmeXK3KCEU=" alt="" width="1500" height="300" >
+</td>
+<!---->
+
+    
+<!--Boostrap-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>   
+</body>
+
+</html>
